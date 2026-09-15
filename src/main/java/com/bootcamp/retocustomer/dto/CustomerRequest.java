4898ea38e53eb2f7cd98455d3697ff01d0f6337c
LDK-R45TT <likkillah.8800@gmail.com>
2026-09-14 23:02:36 -0500

    [response y request] constructores

--- src/main/java/com/bootcamp/retocustomer/dto/CustomerRequest.java
+++ src/main/java/com/bootcamp/retocustomer/dto/CustomerRequest.java
@@ -17,4 +17,46 @@ public class CustomerRequest {
     @NotNull
     @Max(value = 90, message = "el cliente debe tener maximo 90 años")
     private Integer age;
+
+    public CustomerRequest() {
+    }
+
+    public CustomerRequest(String dni, String name, String lastname, Integer age) {
+        this.dni = dni;
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
--- src/main/java/com/bootcamp/retocustomer/dto/CustomerResponse.java
+++ src/main/java/com/bootcamp/retocustomer/dto/CustomerResponse.java
@@ -1,4 +1,41 @@
 package com.bootcamp.retocustomer.dto;
 
 public class CustomerResponse {
+
+    private String dni;
+    private String fullname;
+    private String age;
+
+    public CustomerResponse() {
+    }
+
+    public CustomerResponse(String dni, String fullname, String age) {
+        this.dni = dni;
+        this.fullname = fullname;
+        this.age = age;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
