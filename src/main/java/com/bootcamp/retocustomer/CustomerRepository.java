85762725798ca0f20e74e805a2698f2aabfc84ab
LDK-R45TT <likkillah.8800@gmail.com>
2026-09-14 23:11:49 -0500

    customer-repository creado

---
+++ src/main/java/com/bootcamp/retocustomer/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.bootcamp.retocustomer;
+
+import com.bootcamp.retocustomer.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long>{
+}
